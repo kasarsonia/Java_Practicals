f977b4dcd5e28cbb0a694169121d441728a130a4
soniakasar <soniakasar@fedora>
2023-01-03 10:21:53 +0530

    ab

--- Prac_2231/src/prac2_class/Admission.java
+++ Prac_2231/src/prac2_class/Admission.java
@@ -1,13 +1,20 @@
 package prac2_class;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Admission {
 
 
 	public static void main(String[] args) {
 	
-		String date = "15/9/2022";
+		Date date = new Date();
+		SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy");
+		String stringDate = DateFor.format(date);
+		System.out.print(stringDate+"\n");
+		String d = "15/9/2022";
 		student s = new student();
-		s.display(date);
+		s.display(d);
 		//student b = new student();
 	}
 }
